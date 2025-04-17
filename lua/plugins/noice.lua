afbeca8d3cb0007d24c9b4ed02829f32eb119d73
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-04-17 16:10:20 -0400

    Windows border for hover

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      views = {
+        hover = {
+          border = {
+            style = "double", -- Esto es para añadir bordes a los hover g
+          },
+        },
+      },
+    },
+  },
+}
