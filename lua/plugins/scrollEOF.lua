776581f17350af658365d369ea57c24cac4db2f4
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-06-15 00:16:15 -0400

    Fix scroll on terminal too

--- lua/plugins/scrollEOF.lua
+++ lua/plugins/scrollEOF.lua
@@ -1,5 +1,9 @@
 return {
   "Aasim-A/scrollEOF.nvim",
-  event = { "CursorMoved", "WinScrolled" },
-  opts = {},
+  event = "InsertEnter", -- Solo cargar cuando entres en insert mode
+  opts = {
+    insert_mode = true,
+    -- Deshabilitar en modo normal y visual
+    disabled_modes = { "n", "v", "V", "\22" },
+  },
 }
