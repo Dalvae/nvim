d084d8e97844e288a0c95af4e0fdddb20c811269
Dalvae <diego.alvarez.e@ug.uchile.cl>
2025-07-06 00:05:31 -0400

    Update and fix to blink-cmp.lua

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -10,10 +10,9 @@ return {
   dependencies = {
     "rafamadriz/friendly-snippets",
     "L3MON4D3/LuaSnip",
-    -- add blink.compat to dependencies
     {
       "saghen/blink.compat",
-      optional = true, -- make optional so it's only enabled if any extras need it
+      optional = true,
       opts = {},
       version = not vim.g.lazyvim_blink_main and "*",
     },
@@ -29,17 +28,11 @@ return {
       end,
     },
     appearance = {
-      -- sets the fallback highlight groups to nvim-cmp's highlight groups
-      -- useful for when your theme doesn't support blink.cmp
-      -- will be removed in a future release, assuming themes add support
       use_nvim_cmp_as_default = false,
-      -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-      -- adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
     },
     completion = {
       accept = {
-        -- experimental auto-brackets support
         auto_brackets = {
           enabled = true,
         },
@@ -58,12 +51,7 @@ return {
       },
     },
 
-    -- experimental signature help support
-    -- signature = { enabled = true },
-
     sources = {
-      -- adding any nvim-cmp sources here will enable them
-      -- with blink.compat
       compat = {},
       default = { "lsp", "path", "snippets", "buffer" },
     },
@@ -77,6 +65,7 @@ return {
       ["<C-y>"] = { "select_and_accept" },
     },
   },
+
   ---@param opts blink.cmp.Config | { sources: { compat: string[] } }
   config = function(_, opts)
     -- setup compat sources
@@ -92,22 +81,6 @@ return {
       end
     end
 
-    -- add ai_accept to <Tab> key
-    if not opts.keymap["<Tab>"] then
-      if opts.keymap.preset == "super-tab" then -- super-tab
-        opts.keymap["<Tab>"] = {
-          require("blink.cmp.keymap.presets")["super-tab"]["<Tab>"][1],
-          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-          "fallback",
-        }
-      else -- other presets
-        opts.keymap["<Tab>"] = {
-          LazyVim.cmp.map({ "snippet_forward", "ai_accept" }),
-          "fallback",
-        }
-      end
-    end
-
     -- Unset custom prop to pass blink.cmp validation
     opts.sources.compat = nil
 
@@ -119,13 +92,9 @@ return {
         local kind_idx = #CompletionItemKind + 1
 
         CompletionItemKind[kind_idx] = provider.kind
-        ---@diagnostic disable-next-line: no-unknown
         CompletionItemKind[provider.kind] = kind_idx
 
-        ---@type fun(ctx: blink.cmp.Context, items: blink.cmp.CompletionItem[]): blink.cmp.CompletionItem[]
         local transform_items = provider.transform_items
-        ---@param ctx blink.cmp.Context
-        ---@param items blink.cmp.CompletionItem[]
         provider.transform_items = function(ctx, items)
           items = transform_items and transform_items(ctx, items) or items
           for _, item in ipairs(items) do
@@ -135,7 +104,6 @@ return {
           return items
         end
 
-        -- Unset custom prop to pass blink.cmp validation
         provider.kind = nil
       end
     end
